8087b590a4f90045491c83055bf992fe395baa71
AlanDufail <alan.dufail@mail-ecv.fr>
2022-04-13 14:09:33 +0200

    initial push

---
+++ main.js
@@ -0,0 +1,12 @@
+const API_KEY = 'NGVkN2E4MjYtYWY2Ni00ODY5LTk2NjgtMzY3MTFlMGJkY2Mz';
+
+const URLS = {
+  BASE_URL: "https://api.m3o.com/v1/nft/Assets",
+
+}
+
+const content = document.querySelector('#content');
+const search = document.querySelector('#search');
+const previous = document.querySelector('#prev')
+const next = document.querySelector('#next')
+const current = document.querySelector('#current')
